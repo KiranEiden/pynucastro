e7586d052f956292e61427ccc8ac68b8ebf52a2f
KiranEiden <KiranEiden@users.noreply.github.com>
2026-01-22 05:04:29 +0000

    deploy: 62b70cb9484c6bf79c0b5ff21ab972e2b0a61e4c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,13 +1,12 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0.post412+gad4d85c',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.4.0.post963+g62b70cb94',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
+    SOURCELINK_SUFFIX: '',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
